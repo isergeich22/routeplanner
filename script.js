96c0dfea0164da54957f48b544f7e825f96f7d41
ilya <skiing1r@gmail.com>
2023-03-19 20:14:24 +0300

    added autocounting leftover

--- script.js
+++ script.js
@@ -4,13 +4,21 @@ const content = document.querySelector('.route-list')
 const finalSum = document.querySelector('.finalsum')
 const total = document.querySelector('#finalsum')
 const startsum = document.querySelector('#startsum')
+const finalRoad = document.querySelector('#finalroad')
+const getRoad = document.querySelector('#get-road')
 const getStart = document.querySelector('#get-start')
 const getFinal = document.querySelector('#get-final')
+const leftOver = document.querySelector('#finalleftover')
+const getLeftover = document.querySelector('#get-leftover')
+const removeStart = document.querySelector('#remove-start')
+const removeRoad = document.querySelector('#remove-road')
 const lsKey = 'POINT'
+const lsKey_ = 'ROAD'
 const _lsKey = 'COIN'
 
 const points = getState()
 const coins = getStateCoins()
+const roads = getStateRoads()
 
 const date_ob = new Date()
 
@@ -20,16 +28,72 @@ let month = ''
 date_ob.getDate() - 10 < 0 ? day = `0${date_ob.getDate()}`: day = date_ob.getDate()
 date_ob.getMonth() - 10 < 0 ? month = `0${date_ob.getMonth() + 1}`: month = date_ob.getMonth()+1
 
+removeStart.addEventListener('click', () => {
+    localStorage.removeItem(_lsKey)
+    startsum.value = '0'
+    getStart.disabled = false
+})
+
+removeRoad.addEventListener('click', () => {
+    localStorage.removeItem(lsKey_)
+    finalRoad.value = '0'
+    getRoad.disabled = false
+})
+
 getStart.addEventListener('click', () => {
     createCoin(startsum.value)
 
     saveStateCoins()
+
+    init()
 })
+
+getRoad.addEventListener('click', () => {
+    createRoad(finalRoad.value)
+
+    saveStateRoads()
+
+    init()
+})
+
 addButton.addEventListener('click', () => {
     createPoint(input.value)
 
     saveState()
 })
+
+function createRoad(text) {
+    if(text == '') {
+        finalRoad.classList.toggle('invalid')
+
+        setTimeout(() => {
+            finalRoad.classList.remove('invalid')
+        }, 3000)
+    } else {
+        const newRoad = {
+            number: parseInt(text),
+            date: `${day}/${month}`
+        }
+
+        finalRoad.value = ''
+
+        roads.push(newRoad)
+        saveStateRoads()
+        init()
+
+    }
+}
+
+function renderRoads() {
+    if(roads.length == 0) {
+        finalRoad.value = '0'
+    } else {
+        for(i = 0; i < roads.length; i++) {
+            finalRoad.value = `${roads[i].number}`
+            getRoad.disabled = true
+        }
+    }
+}
 
 function createCoin(text) {
     if (text == '') {
@@ -47,7 +111,7 @@ function createCoin(text) {
         startsum.value = ''
 
         coins.push(newCoin)
-        saveState()
+        saveStateCoins()
         init()
 
     }
@@ -59,6 +123,7 @@ function renderCoins() {
     } else {
         for (i = 0; i < coins.length; i++) {
             startsum.value = `${coins[i].number}`
+            getStart.disabled = true
         }
     }
 }
@@ -154,6 +219,7 @@ function renderPoints() {
 }
 
 getFinal.addEventListener('click', countFinal)
+getLeftover.addEventListener('click', getingLeftover)
 
 function countFinal() {
     let sum = 0
@@ -166,12 +232,25 @@ function countFinal() {
     total.value = sum
 }
 
+function getingLeftover() {
+    leftOver.value = parseInt(startsum.value) - (parseInt(total.value) + parseInt(finalRoad.value))
+}
+
 function togglePoint(event) {
     const text = event.target.dataset.point
     const point = points.find(p => p.point_name === text)
     point.done = event.target.checked
     saveState()
     init()
+}
+
+function saveStateRoads() {
+    localStorage.setItem(lsKey_, JSON.stringify(roads))
+}
+
+function getStateRoads() {
+    const row = localStorage.getItem(lsKey_)
+    return row ? JSON.parse(row) : []
 }
 
 function saveStateCoins() {
@@ -195,6 +274,7 @@ function getState() {
 function init() {
     renderPoints()
     renderCoins()
+    renderRoads()
 }
 
 init()
